50f2a019ff0048c1b905d9322df03b4597de6dfa
Allen Phillip Pottinger <allenpottinger@gmail.com>
2019-12-21 23:55:15 +0000

    Done.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Say Hello
+whay# Say Hello
 
 ## Objectives
 
--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,6 @@
-# Build your say_hello method here
+def greeting(name = "Ruby Programmer")
+  puts "Hello #{name}"
+end
+
+say_hello("Allen")
+
